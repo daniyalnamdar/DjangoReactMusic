0fbe7288c1dd28b93204e2ed7cb0a9c7127fdb9e
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2024-03-01 14:57:45 +0300

    changes so far episode 8 done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # added ones
     "api.apps.ApiConfig",
-    "rest_framework"
+    "frontend.apps.FrontendConfig",
+    "rest_framework",
+    
 ]
 
 MIDDLEWARE = [
